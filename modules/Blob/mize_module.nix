3fc5c4a1faba733790a22b22dc795238d569d508
Sebastian Moser <sebastian@c2vi.dev>
2024-08-31 15:27:46 +0200

    blob add selector_string

--- modules/Blob/mize_module.nix
+++ modules/Blob/mize_module.nix
@@ -1,6 +1,11 @@
 { craneLib
+, toolchain_version
 , ...
 }:
 craneLib.buildPackage {
   src = ./.;
+  selector_string = builtins.toJSON {
+    inherit toolchain_version;
+    name = "Blob";
+  };
 }
