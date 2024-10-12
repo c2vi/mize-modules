c4ba62aa7d47a9cfea8f1ca71e6811d52944da86
Sebastian Moser <sebastian@c2vi.dev>
2024-10-12 15:09:23 +0200

    test

--- mize_module.nix
+++ mize_module.nix
@@ -9,15 +9,17 @@ externals = [
     owner = "c2vi";
     repo = "mme";
     rev = "a04f4df5ce8e3b1a8d024320176fab84a4f4e8ed";
-    hash = "";
+    hash = "sha256-vtcbIPypT4pRu6EwgKxWX/KjoAAYQHivQnZQRwY947M=";
   })
 
+  /*
   (fetchFromGitHub {
     owner = "c2vi";
     repo = "victorinix";
     rev = "690aa83bc3b37263b1aeab61b3bd8f84960e0b1e";
     hash = "sha256-Xj/aESN3yeMMVfmL05HXt7gMLv+W4yNB8RcNSk6FweQ=";
   })
+  */
 
 ];
 
